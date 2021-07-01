bb2f4f2132631dfa14f172d2de1f8c7872281dbd
Piotr Kedra <piotr.k9750@gmail.com>
2021-07-01 14:52:57 +0200

    add axios request

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,14 +3,24 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import axios from "axios"
 
-const SecondPage = () => (
-  <Layout>
-    <Seo title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const SecondPage = () => {
+
+  const [dupa, setDupa] = React.useState("dupa is loading...");
+  axios.get(`https://jsonplaceholder.typicode.com/users/1`)
+    .then(res => {
+      console.log(res)
+      setDupa(res.data.name + ' - ' + res.data.email)
+    })
+  return (
+    <Layout>
+      <Seo title="Page two" />
+      <h1>Hi from the second page</h1>
+      <p>{dupa}</p>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+}
 
 export default SecondPage
